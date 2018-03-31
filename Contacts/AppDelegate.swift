bf36d214f1c9a9d4a885f48e387fa2b3a563f9ea
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-31 00:05:14 -0400

    use contact detail con instead

--- Contacts/AppDelegate.swift
+++ Contacts/AppDelegate.swift
@@ -51,7 +51,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         
         
         guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? ContactDetailController else { return false }
         
         if topAsDetailController.detailItem == nil {
             // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.

882f8d3f93f4002a2a6c2b3dcc07f6096e39f176
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-30 23:40:09 -0400

    Contact

---
+++ Contacts/Contact.swift
@@ -0,0 +1,24 @@
+//
+//  Contact.swift
+//  Contacts
+//
+//  Created by Arshin Jain on 3/30/18.
+//  Copyright © 2018 Arshin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Contact {
+    let firstName: String
+    let lastName: String
+    let phone: String
+    let email: String
+    let street: String
+    let city: String
+    let state: String
+    let zip: String
+    let image: UIImage?
+    var favorite: Bool
+}
+

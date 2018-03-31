e48ea18ef6f82202b70544d5f85640e302ffa8a9
Arshin Jain <arshinx@users.noreply.github.com>
2018-03-31 00:23:47 -0400

    create and return cell

--- Contacts/ContactListController.swift
+++ Contacts/ContactListController.swift
@@ -20,12 +20,23 @@ class ContactListController: UITableViewController {
 
     // MARK: - Table view data source
 
+    // sections = 1
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
+    // rows = # of contacts
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contacts.count
+    }
+    
+    // call for each row
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create re-usable cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
+        let contact = contacts[indexPath.row]
+        cell.textLabel?.text = contact.firstName
+        return cell
     }
 
 }

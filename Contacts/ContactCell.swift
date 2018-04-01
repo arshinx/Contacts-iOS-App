41eb8c048776cc65319a322862b521b31a29b484
Arshin Jain <arshinx@users.noreply.github.com>
2018-04-01 00:30:28 -0400

    Contact Cell

---
+++ Contacts/ContactCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactCell.swift
+//  Contacts
+//
+//  Created by Arshin Jain on 4/1/18.
+//  Copyright © 2018 Arshin. All rights reserved.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
